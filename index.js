1c517544006019614c0754a860985e1d284084f8
Karl Lew <karl@firepick.org>
2018-04-02 07:19:31 -0700

    remove blockchain

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 module.exports = {
-    AbstractBlock: require('./src/block').AbstractBlock,
-    Agent: require('./src/agent'),
     Asset: require("./src/asset"),
-    Block: require('./src/block'),
-    OyaChain: require("./src/oya-chain"),
     Filter: require("./src/filter"),
     Inventory: require("./src/inventory"),
     Plant: require("./src/plant"),
     Query: require('./src/query'),
     RbAsset: require('./src/rb-asset'),
-    SerializedKeyPair: require('./src/serialized-key-pair'),
     TValue: require("./src/tvalue"),
-    Transaction: require('./src/transaction'),
 
 };
