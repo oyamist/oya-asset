2e27b3d925781e079ddad173abb481631dce1b24
Karl Lew <karl@firepick.org>
2018-04-26 23:16:15 -0400

    standalone ui

--- src/ui/oya-asset.vue
+++ src/ui/oya-asset.vue
@@ -10,28 +10,31 @@
     </rb-about>
 
     <v-card>
-        <v-card-title primary-title>
-            {{tvalue('id')}} {{tvalue('name')}} 
+        <v-card-title primary-title class="headline">
+            {{attrValue('name')}} 
             <v-spacer/>
-            {{$route.query.guid}}
         </v-card-title>
         <v-card-text>
-            <table class="pl-3">
+            <table class="pl-3" cellpadding=0 cellspacing=0>
                 <tr>
-                    <th>Property</th>
-                    <th>Value</th>
-                    <th>Effective</th>
-                    <th>Notes</th>
+                    <th class="attr-header">Category</th>
+                    <th class="attr-header">Property</th>
+                    <th class="attr-header">Value</th>
+                    <th class="attr-header">Effective</th>
+                    <th class="attr-header">Notes</th>
                 </tr>
-                <tr v-for="(tv,i) in tvalues" :key="i">
-                    <td class="tvalue-header">
-                        <span v-show="i===0 || tvalues[i-1].tag !== tvalues[i].tag" >
-                            {{tv.tag}}
-                        </span>
+                <tr v-for="(attr,i) in attrs" :key="i" class="attr-row">
+                    <td class="attr-category" v-if="i===0 || attrs[i-1].category !== attrs[i].category" >
+                        {{attr.category}}
                     </td>
-                    <td> {{tv.value}} </td>
-                    <td > <span class="tvalue-date" >{{tvDate(tv.t)}}</span> </td>
-                    <td> {{tv.text}} </td>
+                    <td class="attr-category-empty" v-else> &nbsp; </td>
+                    <td class="attr-header" v-if="i===0 || attrs[i-1].tag !== attrs[i].tag" >
+                        {{attr.tag}}
+                    </td>
+                    <td class="attr-header-empty" v-else> &nbsp; </td>
+                    <td> {{attr.value}} </td>
+                    <td > <span class="attr-date" >{{attrDate(attr.t)}}</span> </td>
+                    <td> {{attr.text}} </td>
                 </tr>
             </table>
         </v-card-text>
@@ -43,6 +46,8 @@
 
 import Vue from 'vue';
 import rbvue from "rest-bundle/index-vue";
+
+const RETROACTIVE = new Date(-8640000000000000); // Javascript minimum date
 
 export default {
     mixins: [ 
@@ -62,6 +67,7 @@ export default {
             asset: {
                 tvalues: [],
             },
+            attrs: [],
         }
     },
     methods: {
@@ -71,32 +77,59 @@ export default {
             console.log(`refresh(})`, this.$route.query);
             this.$http.get(url).then(res=>{
                 console.log('res',res);
-                this.asset = res.data;
-                this.asset.tvalues.forEach(tv => (tv.t = new Date(tv.t)));
-                this.asset.tvalues.sort((a,b) => {
-                    if (a.tag === b.tag) {
-                        if (a.t.getTime() === b.t.getTime()) {
-                            return 0;
-                        }
-                        return a.t.getTime() < b.t.getTime() ? 1 : -1;
+                var asset = this.asset = res.data;
+                var tvalues = asset.tvalues || [];
+                tvalues.forEach(tv => (tv.t = new Date(tv.t)));
+                var attrs = this.attrs = tvalues.map(tv => {
+                    var attr = Object.assign({
+                        category: tv.tag === 'id' || tv.tag === 'name' 
+                            ? 'Identity' : 'History',
+                    }, tv);
+                    return attr;
+                });
+                Object.keys(asset).forEach(key => {
+                    if (key !== 'tvalues') {
+                        attrs.push({
+                            category: key === 'guid' ? "Identity" : "Detail",
+                            tag: key,
+                            value: asset[key],
+                            t: RETROACTIVE,
+                        });
+                        
                     }
-                    return a.tag.localeCompare(b.tag);
+                });
+                attrs.sort((a,b) => {
+                    if (a.category === b.category) {
+                        if (a.tag === b.tag) {
+                            if (a.t.getTime() === b.t.getTime()) {
+                                return 0;
+                            }
+                            return a.t.getTime() < b.t.getTime() ? 1 : -1;
+                        }
+                        return a.tag.localeCompare(b.tag);
+                    }
+                    var order = {
+                        Identity: 1,
+                        Detail: 2,
+                        History: 3,
+                    };
+                    return order[a.category] < order[b.category] ? -1 : 1;
                 });
             }).catch(e=>{
                 console.error(e);
             });
         },
-        tvalue(tag) {
-            var tvalues = this.tvalues;
-            for (var i=tvalues.length; i-- > 0;) {
-                var tv = tvalues[i];
-                if (tv && tv.tag === tag) {
-                    return tv.value;
+        attrValue(tag) {
+            var attrs = this.attrs;
+            for (var i=attrs.length; i-- > 0;) {
+                var attr = attrs[i];
+                if (attr && attr.tag === tag) {
+                    return attr.value;
                 }
             }
             return undefined;
         },
-        tvDate(t) {
+        attrDate(t) {
             var msday = 24 * 3600 * 1000;
             var d = Math.round((t-Date.now()) / msday);
             if (Math.abs(d) < 365) {
@@ -136,10 +169,28 @@ th {
     text-align: left;
     text-decoration: underline;
 }
-.tvalue-row {
+.attr-row {
     padding-top: 0.2em;
 }
-.tvalue-header {
+.attr-row > td {
+    border-top: 1px solid #eee;
+}
+.attr-category {
+    margin-top: 1em;
+    vertical-align: top;
+    padding-left: 0.5em;
+    padding-right: 0.5em;
+    font-weight: 800;
+    font-style: italic;
+}
+td.attr-category-empty {
+    border-top: none;
+}
+.attr-header {
     font-weight: 500;
+    text-decoration: none;
+}
+td.attr-header-empty {
+    border-top: none;
 }
 </style>
